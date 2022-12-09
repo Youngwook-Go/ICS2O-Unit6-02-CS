d903e84976ec28df7d08907887f93149537f5dd6
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-09 19:41:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program {
+  public static async Task Main()
+    {
+        var count = await File.ReadAllTextAsync(@"Text.txt");
+        string answer;
+        Console.Write("Count will be increased if you say yes\n");
+        Console.Write("Count will not be increased if you say no\n");
+        Console.Write("Please enter the answer : "); answer= Console.ReadLine();
+
+        if (answer == "yes") {
+            count = count + 1;
+            Console.Write("\nYou answered yes\n");
+        } else {
+            Console.Write("\nYou answered no\n");
+        }
+      
+        await File.WriteAllTextAsync("Text.txt", count);
+        Console.WriteLine("The count is : " + count);
+    }
+}
